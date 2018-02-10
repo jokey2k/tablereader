7c73c7575aa569fe54a2477ebea1eef9236514f5
Markus Ullmann <mail@markus-ullmann.de>
2018-02-10 23:01:20 +0100

    Prepare 1.1.0

--- tablereader/__init__.py
+++ tablereader/__init__.py
@@ -29,7 +29,7 @@ import tablereader._csv_from_pypy as _csv
 # constants
 CLEAR_STRING = ""  # used to speed up processing in pypy, has no functional meaning
 
-__version__ = "1.0.3"
+__version__ = "1.1.0"
 
 
 class XLReader(object):
